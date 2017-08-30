762272f35223d5141b2442aade2dc7e05965f1b5
Jay Hongvarivatana <jayyhong@gmail.com>
2017-08-29 17:23:00 -0700

    Added enemies, damage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,26 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   file: any
   start: any
+  damage: any;
+  baseDamage: any;
+  enemyHealth: any;
+  enemies: any;
+  enemyDatabase: any;
   constructor(public navCtrl: NavController) {
     this.file = 1;
+    this.baseDamage = 50;
+    this.damage = 0;
+    this.enemyHealth = 0;
+    this.enemies = ['blob','blob2','blob3']
+    this.enemyDatabase = {
+      'blob': 1000,
+      'blob2': 2000,
+      'blob3': 3000
+    }
+  }
+
+  ngOnInit() {
+    this.spawnEnemy();
   }
 
   frame() {
@@ -21,8 +39,26 @@ export class HomePage {
     }
   }
 
+  spawnEnemy() {
+    let random = Math.floor(Math.random() * this.enemies.length)
+    console.log('Enemy: ', this.enemies[random])
+    this.enemyHealth = this.enemyDatabase[this.enemies[random]]
+    console.log('Enemy Health: ', this.enemyHealth)
+  }
+
   test() {
     console.log('test')
+  }
+
+  attack() {
+    this.damage = Math.ceil(Math.random() * 10 + this.damage) + this.baseDamage
+    console.log("- ", this.damage)
+    this.enemyHealth = this.damage - this.enemyHealth;
+    console.log('Enemy Health: ', this.enemyHealth)
+    if (this.enemyHealth <= 0) {
+      console.log('Enemy died')
+      this.spawnEnemy();
+    }
   }
 
   animate() {
@@ -30,7 +66,7 @@ export class HomePage {
       this.file = 7;
       clearInterval(this.start)
     }
-    this.start = setInterval(()=>{
+    this.start = setInterval(() => {
       this.frame();
     }, 24)
   }
